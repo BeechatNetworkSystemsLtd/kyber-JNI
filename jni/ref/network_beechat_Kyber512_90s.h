4eef4367d21c343f085d97aa87f208dc368e4352
Beechat Network Systems Ltd <7268708+BeechatNetworkSystemsLtd@users.noreply.github.com>
2021-02-12 14:13:33 +0700

    Add a native implementation

---
+++ jni/ref/network_beechat_Kyber512_90s.h
@@ -0,0 +1,36 @@
+#include <jni.h>
+/* Header for class network_beechat_Kyber512_90s */
+
+#ifndef _Included_network_beechat_Kyber512_90s
+#define _Included_network_beechat_Kyber512_90s
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+ * Class:     network_beechat_Kyber512_90s
+ * Method:    crypto_kem_keypair
+ * Signature: ([C[C)I
+ */
+JNIEXPORT jint JNICALL Java_network_beechat_Kyber512_190s_crypto_1kem_1keypair
+  (JNIEnv *, jobject, jcharArray, jcharArray);
+
+/*
+ * Class:     network_beechat_Kyber512_90s
+ * Method:    crypto_kem_enc
+ * Signature: ([C[C[C)I
+ */
+JNIEXPORT jint JNICALL Java_network_beechat_Kyber512_190s_crypto_1kem_1enc
+  (JNIEnv *, jobject, jcharArray, jcharArray, jcharArray);
+
+/*
+ * Class:     network_beechat_Kyber512_90s
+ * Method:    crypto_kem_dec
+ * Signature: ([C[C[C)I
+ */
+JNIEXPORT jint JNICALL Java_network_beechat_Kyber512_190s_crypto_1kem_1dec
+  (JNIEnv *, jobject, jcharArray, jcharArray, jcharArray);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
